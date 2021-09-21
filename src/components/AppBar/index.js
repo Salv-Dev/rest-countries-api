dc85613c47ea8e6b8ed5e6243e3e099f865629b9
Salv-Dev <47800892+Salv-Dev@users.noreply.github.com>
2021-09-20 21:42:44 -0300

    appbar component (#1)

---
+++ src/components/AppBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function AppBar() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default AppBar;
---
+++ src/components/AppBar/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    width: 100%;
+    min-height: 50px;
+    background-color: white;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.1), 0 3px 6px rgba(0,0,0,0.1);
+`;
