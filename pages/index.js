70bb5edf5b30ba1b15b481b2c467cc17303f2cf3
Salv-Dev <47800892+Salv-Dev@users.noreply.github.com>
2021-09-21 22:33:15 -0300

    theme provider config, add theme to component styles (#4)

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import ToolBar from '@/components/ToolBar';
 import Head from 'next/head';
 import AppBar from 'src/components/AppBar';
 import { IoMoonOutline } from 'react-icons/io5';
+import { IoSunnyOutline } from 'react-icons/io5';
 
-const Home = () => (
+const Home = ({ setIsDarkMode, isDarkMode }) => (
   <div>
     <Head>
       <title>Rest Countries API</title>
@@ -13,7 +14,12 @@ const Home = () => (
 
     <AppBar>
       <ToolBar title="Where in the world?">
-        <Button startIcon={<IoMoonOutline />}>Dark Mode</Button>
+        <Button 
+          startIcon={isDarkMode ? <IoSunnyOutline size={18} /> : <IoMoonOutline size={18} />}
+          onClick={() => setIsDarkMode(prev => !prev)}
+        >
+          {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+        </Button>
       </ToolBar>
     </AppBar>
   </div>
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -7,14 +7,15 @@ export const Container = styled.button`
     padding: 10px;
     gap: 10px;
     border: none;
-    background: transparent;
+    color: ${props => props.theme.text};
+    background-color: ${props => props.theme.element};
     box-shadow: ${props => props.variant == 'shaded' ? '0 3px 6px rgba(0,0,0,0.1), 0 3px 6px rgba(0,0,0,0.1)':'none'};
-    transition: background-color .2s ease-in-out;
+    transition: all .2s ease-in-out;
     cursor: pointer;
 
     &:hover,
     &:active {
-        background-color: hsl(0, 0%, 98%); 
+        background-color: ${props => props.theme.background}; 
     }
 `;
 
