5aa78a9827bd6a4585447363fa340ded577bff73
Salv-Dev <salv.dev.br@gmail.com>
2021-09-21 21:16:52 -0300

    toolbar component styles

--- src/components/ToolBar/index.js
+++ src/components/ToolBar/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
-function ToolBar() {
+function ToolBar({ title, children }) {
   return (
-      <Container></Container>
+      <Container>
+        {title && <Title>{title}</Title>}
+        {children}
+      </Container>
   );
 }
 
