06329980f6a199899d17a6ee8f6af9e7ff93a38d
Salv-Dev <salv.dev.br@gmail.com>
2021-09-29 18:02:38 -0300

    CardContent component

--- src/components/ui/Box/styles.js
+++ src/components/ui/Box/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    gap: 10px;
+    gap: 40px;
 
     @media(min-width: 600px) {
         flex-direction: row;
